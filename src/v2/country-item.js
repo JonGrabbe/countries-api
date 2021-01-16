1773d0acb5d53c5b0d5e99d126ebb51f53b3ff90
Jon <jongrabbe@outlook.com>
2021-01-16 01:20:35 -0800

    population number now formated with commas

--- src/v2/country-item.js
+++ src/v2/country-item.js
@@ -1,11 +1,18 @@
 export default function CountryItem(props) {
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    let population;
+    if(props.items.population) {
+        population = numberWithCommas(props.items.population)
+    }
     return (
         <div className="country-thumbnail">
             <div className="country-thumbnail__img-container">
                 <img src={props.items.flag} alt=""/>
             </div>
             <h2>{props.items.name}</h2>
-            <p>Population: {props.items.population}</p>
+            <p>Population: {population}</p>
             <p>Region: {props.items.region}</p>
             <p>Capital: {props.items.capital}</p>
         </div>
